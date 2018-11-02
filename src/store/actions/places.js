48fca42ce4d3c6ca857ad20d066ff9f83a1cb3f8
sean <sean@seanui-iMac.local>
2018-11-02 18:28:35 +0900

    add firebase

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,11 +1,31 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
  
 export const addPlace = (placeName, location, image) => {    
-    return {
-        type: ADD_PLACE,
-        placeName : placeName,
-        location: location,
-        image: image
+    return dispatch => {
+        const placeData = {
+            placeName: placeName,
+            location: location           
+        };
+        fetch("https://us-central1-rncourse-e3f56.cloudfunctions.net/storeImage", {
+            method: "POST",
+            body: JSON.stringify({
+                image: image.base64
+            })
+        })
+        .catch(err => console.log(err))
+        .then(res => res.json())
+        .then(parsedRes => 
+            console.log(parsedRes)
+        )
+        // fetch("https://rncourse-e3f56.firebaseio.com/places.json", {
+        //     method: "POST",
+        //     body: JSON.stringify(placeData)
+        // })
+        // .catch(err => console.log(err))
+        // .then(res => res.json())
+        // .then(parsedRes => {
+        //     console.log(parsedRes);
+        // });
     };
 };
 
